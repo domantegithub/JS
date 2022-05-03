5cdcbdb7c022593532181022b5bf9149abdb9c3c
domantegithub <domante.nav@gmail.com>
2022-05-03 18:12:42 +0300

    home work ternary and switch

--- 3Paskaita/script.js
+++ 3Paskaita/script.js
@@ -69,9 +69,92 @@
 
 // console.log(weather);
 
-//Template string, juos naudoti tik tada, kai dedame kintamuosius į vidų.
-const myName = "Domante";
-const surName = "Navarskaite";
+// //Template string, juos naudoti tik tada, kai dedame kintamuosius į vidų.
+// const myName = "Domante";
+// const surName = "Navarskaite";
 
-console.log(myName + " " + surName);
-console.log(`${myName} ${surName}`); 
+// console.log(myName + " " + surName);
+// console.log(`${myName} ${surName}`); 
+
+// NAMU DARBAI
+const car="Mini";
+
+switch (car) {
+    case "VW":
+    case "Audi":
+    case "Bentley":
+    case "Bugatti":
+    case "Lamborghini":
+    case "Porsche":
+    console.log("VW grupė");
+    break;
+    case "BMW":
+    case "Mini":
+    case "Rolls-Royce":
+    console.log("BMW grupė")
+    break;
+    default:
+    console.log("Nei VW nei BMW")
+}
+
+const userInput = "lalal";
+
+switch (userInput) {
+    case "Obuolys":
+    case "Agrastas":
+    case "Avietė":
+    case "Braškė":
+    case "Agurkas":
+    console.log("Vaisiai");
+    break;
+    case "Bulvė":
+    case "Rabarbaras":
+    case "Svogūnas":
+    case "Poras":
+    case "Kopūstas":
+    console.log("Daržovės")
+    break;
+    default:
+    console.log("Tokio vaisiaus ir daržovės nėra")
+}
+
+const weekDay = 0;
+
+switch (weekDay) {
+case 0:
+console.log("Pirmadienis");
+break;
+case 1:
+console.log("Antradienis");
+break;
+case 2:
+console.log("Treciadienis");
+break;
+case 3:
+console.log("Ketvirtadienis");
+break;
+case 4:
+console.log("Penktadienis");
+break;
+case 5:
+console.log("Šeštadienis");
+case 6:
+console.log("Sekmadienis");
+break;
+default:
+console.log("Bloga savaitės diena");
+break;
+}
+
+//2Uzduotis. Ternary
+const myName="Domante";
+const answer=myName.length>5 ? "Long name" : "Short name"
+console.log(answer);
+
+const clientAge=-1;
+const leagalAge=18;
+
+// const isAbleToDrive= clientAge>=leagalAge ? "Can Drive" : "Can't drive"
+
+const isAbleToDrive= clientAge < 0 || clientAge > 120 ? "Invalid age" : clientAge>=leagalAge ? "Can Drive" : "Can't drive" 
+console.log(isAbleToDrive);
