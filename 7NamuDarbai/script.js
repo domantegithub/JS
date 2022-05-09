fd26dc411467658051bdefb238f63a2409b89695
domantegithub <domante.nav@gmail.com>
2022-05-09 22:17:56 +0300

    events and forms round 2

--- 7NamuDarbai/script.js
+++ 7NamuDarbai/script.js
@@ -1,14 +1,19 @@
-const submitForm = document.getElementById("my-form");
-submitForm.addEventListener("submit", myFunction);
+const ageInput = document.getElementById("age");
+const form = document.querySelector("form");
+const priceDisplay = document.getElementById("price-result");
 
-function myFunction(event) {
-    event.preventDefault();
-    const myNameInput = document.querySelector("input[name=name]");
-    const myAge=document.querySelector("input[name=age]")
+function handleFormSubmit(event) {
+  event.preventDefault()
+  const price = 6;
+  const age = Number(ageInput.value);
 
-    const result = document.getElementById("age-result");
-    result.textContent=myAge;
-
-    myNameInput.value="";
-    myAge.value = "";
+  if (age >= 60) {
+    priceDisplay.textContent = (0.5 * price).toFixed(2);
+  } else if (age < 18) {
+    priceDisplay.textContent = (0.45 * price).toFixed(2);
+  } else {
+    priceDisplay.textContent = price.toFixed(2);
+  }
 }
+
+form.addEventListener("submit", handleFormSubmit);
