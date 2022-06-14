e165bcb82680e0c9be98f8836886e2c46aebc70b
domantegithub <domante.nav@gmail.com>
2022-06-14 18:00:45 +0300

    fetch comments, users lists

--- 23Paskaita/script.js
+++ 23Paskaita/script.js
@@ -18,20 +18,59 @@ const url = "https://jsonplaceholder.typicode.com/comments";
 //Pirmas then reikalingas, kad praliestume per json, o antras jau istikruju atiduos duomenis
 //catch butinas, jei negautume duomenu, kad ismestu errora
  fetch(url)
- .then(resp => resp.json())
- .then(resonpse => {
+ .then((resp) => resp.json())
+ .then((resonpse) => {
 //atvaizduoti pirmo elemento duomenis
+const fisrtItem = resonpse[0]; //Po to darom console.log(fisrtItem);
+//atvaizduojam HTML'e email, name ir phone
+const paragraph = document.createElement("p"); //sukuriam elementa "p"
+//nustatome koks bus atvaizduotas turinys, siuo atveju pasirinkome email, name ir phone
+paragraph.textContent = `${fisrtItem.email}, ${fisrtItem.name}, ${fisrtItem.body}`;
+
+//pridedame į HTMl body dalį naują p elementą
+document.body.prepend(paragraph);
      console.log("comments:", resonpse);
- }).catch(error => {
+ })
+ .catch(error => {
      console.error(error , "Failed to load comments");
  });
 
- const users = "https://jsonplaceholder.typicode.com/users";
 
- fetch(users).then(resp => resp.json()).then(resonpse => {
-     console.log("users:", resonpse);
-     //atvaizduoti pirmo objekto adresa, company ir email, juos sudeti i atskirus kintamuosius
- }).catch(error => {
+ //2UZDUOTIS
+ //atvaizduoti pirmo objekto adresa, company ir email, juos sudeti i atskirus kintamuosius
+ //Veliau juos atvaizduoti HTML'e
+
+ //antra uzduoties dalis, susikuriame kintamaji, kuri inconsolinsim i html
+const renderUser = (user) => {
+    //issitraukiam reiksmes kokiu mums reikia
+    const addressCity = user.address.city;
+    const companyName = user.company.name;
+    const email = user.email;
+
+    const tdAddressCity = document.createElement("td");
+    const tdCompanyName = document.createElement("td");
+    const tdEmail = document.createElement("td");
+
+    //dabar padarome, kad reiksmes butu atvaizduotos text formatu
+    tdAddressCity.textContent = addressCity;
+    tdCompanyName.textContent = companyName;
+    tdEmail.textContent = email;
+
+    //atvaizduojame i lentele tris kintamuosius
+    const trUser = document.createElement("tr");
+    trUser.append(tdAddressCity,tdCompanyName,tdEmail);
+
+    //dabar viska sukeliam i tablebody
+    document.getElementById('users').append(trUser);
+};
+
+ fetch("https://jsonplaceholder.typicode.com/users")
+ .then((resp) => resp.json())
+ .then((resonpse) => {
+const fisrtUser = resonpse[0];
+renderUser(fisrtUser);
+ })
+ .catch(error => {
      console.error(error, ":Failed to load users");
  });
 
